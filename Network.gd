e5e3a124fa3caae389a1b07f61a34a2873b38b79
Frank Berthold <fberthold@kyruus.com>
2020-08-17 10:32:56 -0400

    Updated with better documentation

--- Network.gd
+++ Network.gd
@@ -1,30 +1,42 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var peer
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
-
-
 func lobby_connect():
+    # The `fetch` object will be used to get a list of all the lobbies.
     var fetch = GotmLobbyFetch.new()
+    # All of the lobbies that are currently available for your game.
     var lobbies = yield(fetch.first(), "completed")
+    # The `peer` object will contain the network interface that other games
+    #  will use to contact yours.
+    var peer
+    # For this sample, we'll only be using one lobby. If there are any lobbies
+    #  in the list, then there is already a host.
     if lobbies:
+        # So we'll take the first (and only) lobby in the list.
         var lobby = lobbies[0]
+        # and join it.
         var success = yield(lobby.join(), "completed")
+        # By creating `NetworkedMultiplayerENet` we give our game access to high
+        #  level networking functions like `rpc`
         peer = NetworkedMultiplayerENet.new()
+        # Since the lobby already existed, we know there is a `Gotm.lobby.host`, and
+        #  we can connect to it as a client to the host's ipAddress/port.
         peer.create_client(Gotm.lobby.host.address, 8070)        
     else:
+        # Since there are no lobbies, we'll create a new one.  By setting the first
+        #  parameter to `false` you tell Gotm to not create a share link. This value
+        #  would be true by default.
         Gotm.host_lobby(false)
+        # Notice that we can treat the `Gotm.lobby` just like any other node in our game,
+        #  so we can set it's properties and call methods like normal.
         Gotm.lobby.name = "Color Changer"
+        # If the `Gotm.lobby.hidden` is set to true, then it won't show up when another
+        #  player won't see it when they fetch the lobbies.
         Gotm.lobby.hidden = false
+        # Like above, the host needs to create a `NetworkedMultiplayerENet`
         peer = NetworkedMultiplayerENet.new()
+        # Then create a new server for clients to connect to the servers port. 
         peer.create_server(8070)
+    # For both client & server, then add the `peer` to the node tree, and then
+    #  other systems will be able to connect and send RPC calls to it.
     get_tree().set_network_peer(peer)
 
--- TestScreen.gd
+++ TestScreen.gd
@@ -1,66 +1,74 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    Network.lobby_connect()
-    
+	# Network is a singleton script that's Autoloaded in the project to initiate
+	#  all the connections with GotM's lobby system, and then create newtork
+	#  access through the VPN GotM creates.
+	Network.lobby_connect()
+
+################
+## Networking ##
+################
+	
+# The `remote` keyword tells Godot that other instances of the program
+#  can call this method through `rpc`
 remote func set_color(color):
-    $Background.color = Color(color)
-    
+	$Background.color = Color(color)
+	
+# When the user clicks the 'Send Color' button, then it will make
+#  an `rpc` call which changes the background color on all of the
+#  other screens using the `set_color` method.
+func _on_SendColor_pressed():
+	print("Sending Color: " + str($ColorPickerButton.color))
+	rpc("set_color", $ColorPickerButton.color)
+
+##########################
+## Lobby Status Monitor ##
+##########################
+# The code below is to monitor the player's connection to
+#  GotM and the lobby.
 func on_gotm():
-    if Gotm.is_live():
-        $Fields/IsOnGotM.text = "On GotM: true"
-    else:
-        $Fields/IsOnGotM.text = "On GotM: false"
+	if Gotm.is_live():
+		$Fields/IsOnGotM.text = "On GotM: true"
+	else:
+		$Fields/IsOnGotM.text = "On GotM: false"
 
 func logged_in():
-    if Gotm.user.is_logged_in():
-        $Fields/LoggedIn.text = "Logged In: true"
-    else:
-        $Fields/LoggedIn.text = "Logged In: false"
-        
+	if Gotm.user.is_logged_in():
+		$Fields/LoggedIn.text = "Logged In: true"
+	else:
+		$Fields/LoggedIn.text = "Logged In: false"
+		
 func usernamed():
-    if Gotm.user.is_logged_in():
-        $Fields/Username.text = "Username: " + Gotm.user.display_name
-    else:
-        $Fields/Username.text = "Username: ?"
-        
+	if Gotm.user.is_logged_in():
+		$Fields/Username.text = "Username: " + Gotm.user.display_name
+	else:
+		$Fields/Username.text = "Username: ?"
+		
 func in_lobby():
-    if Gotm.lobby:
-        $Fields/InLobby.text = "In Lobby: " + Gotm.lobby.name
-    else:
-        $Fields/InLobby.text = "In Lobby: ?"
-        
+	if Gotm.lobby:
+		$Fields/InLobby.text = "In Lobby: " + Gotm.lobby.name
+	else:
+		$Fields/InLobby.text = "In Lobby: ?"
+		
 func host_is():
-    if Gotm.lobby:
-        $Fields/IsHost.text = "Host Is: " + Gotm.lobby.host.display_name
-    else:
-        $Fields/IsHost.text = "Host Is: ?"
-        
+	if Gotm.lobby:
+		$Fields/IsHost.text = "Host Is: " + Gotm.lobby.host.display_name
+	else:
+		$Fields/IsHost.text = "Host Is: ?"
+		
 func number_in_lobby():
-    if Gotm.lobby:
-        $Fields/NumberInLobby.text = "Players In Lobby: " + str(len(Gotm.lobby.peers) + 1)
-    else:
-        $Fields/NumberInLobby.text = "Players In Lobby: 0"
+	if Gotm.lobby:
+		$Fields/NumberInLobby.text = "Players In Lobby: " + str(len(Gotm.lobby.peers) + 1)
+	else:
+		$Fields/NumberInLobby.text = "Players In Lobby: 0"
+
+func _process(_delta):
+	on_gotm()
+	logged_in()
+	usernamed()
+	in_lobby()
+	host_is()
+	number_in_lobby()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    on_gotm()
-    logged_in()
-    usernamed()
-    in_lobby()
-    host_is()
-    number_in_lobby()
-
-
-func _on_SendColor_pressed():
-    print("Sending Color: " + str($ColorPickerButton.color))
-    rpc("set_color", $ColorPickerButton.color)
-#    set_color($ColorPickerButton.color)
